ba1a53410b40d4dfe7f3d1ba9b144283e488f2bb
rodina <ra745126@gmail.com>
2025-11-15 14:30:34 +0200

    features

--- lab7_skillForge/src/Backend/User.java
+++ lab7_skillForge/src/Backend/User.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Backend;
 
-/**
- *
- * @author Rodina Mohamed
- */
 public class User {
+    protected String userName;
+    protected String userId;
+    protected String email;
+    protected String passwordHash;
+    protected String role;
+
+    public User(String userName, String userId, String email, String passwordHash, String role) {
+        this.userName = userName;
+        this.userId = userId;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+    
+
+    public String getRole() {
+        return role;
+    }
+    
     
 }
