94b49a823b84cae48a11636c1bef2e0c7c55f698
rodina <ra745126@gmail.com>
2025-11-16 16:51:48 +0200

    courses

--- lab7_skillForge/src/Backend/Courses.java
+++ lab7_skillForge/src/Backend/Courses.java
@@ -9,5 +9,29 @@ package Backend;
  * @author Rodina Mohamed
  */
 public class Courses {
+    private String courseName;
+    private int creditHours;
+
+    public Course(String courseName, int creditHours) {
+        this.courseName = courseName;
+        this.creditHours = creditHours;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
     
+}
 }
